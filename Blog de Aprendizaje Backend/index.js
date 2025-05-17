63e9f083a9553aa4a7c101e2ab3c62a5b8006745
dhernandez-2020416 <dhernandez-2020416@kinal.edu.gt>
2025-05-16 21:52:57 -0600

    Se añande los posts a la db

--- Blog de Aprendizaje Backend/index.js
+++ Blog de Aprendizaje Backend/index.js
@@ -1,7 +1,9 @@
 import { initServer } from './configs/app.js'
 import { config } from 'dotenv'
 import { connect } from './configs/mongo.js'
+import { addPostsToDB } from './helpers/db.validators.js'
 
 config()
 initServer()
 connect()
+addPostsToDB()
